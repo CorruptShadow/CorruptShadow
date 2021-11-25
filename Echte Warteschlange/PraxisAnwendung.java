d07f915100b3ff095bdb587ec2463c95dd06abb2
CorruptShadow <89862346+CorruptShadow@users.noreply.github.com>
2021-11-25 09:33:19 +0100

    Keine doppelte Platzbelkegung
    xyz

--- Echte Warteschlange/PraxisAnwendung.java
+++ Echte Warteschlange/PraxisAnwendung.java
@@ -43,55 +43,56 @@ public class PraxisAnwendung extends EBAnwendung
         hatStift = new Buntstift();
         hatWarteraum = new Warteraum();
         hatErsterPatient = null;
-
+        hatEtikettPlatz = new Etikett[10];
+        
         hatStift.bewegeBis(530,80);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[0] = new Etikett(531, 81, 49, 49, "Stuhl0");
+        hatEtikettPlatz[0] = new Etikett(531, 81, 49, 49, "");
         hatEtikettPlatz[0].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(590,80);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[1]= new Etikett(591, 81, 49, 49, "Stuhl1");
+        hatEtikettPlatz[1]= new Etikett(591, 81, 49, 49, "");
         hatEtikettPlatz[1].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(650,80);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[2] = new Etikett(651, 81, 49, 49, "Stuhl2");
+        hatEtikettPlatz[2] = new Etikett(651, 81, 49, 49, "");
         hatEtikettPlatz[2].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(710,140);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[3] = new Etikett(711, 141, 49, 49, "Stuhl3");
+        hatEtikettPlatz[3] = new Etikett(711, 141, 49, 49, "");
         hatEtikettPlatz[3].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(710,200);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[4] = new Etikett(711, 201, 49, 49, "Stuhl4");
+        hatEtikettPlatz[4] = new Etikett(711, 201, 49, 49, "");
         hatEtikettPlatz[4].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(710,260);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[5] = new Etikett(711, 261, 49, 49, "Stuhl5");
+        hatEtikettPlatz[5] = new Etikett(711, 261, 49, 49, "");
         hatEtikettPlatz[5].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(710,320);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[6] = new Etikett(711, 321, 49, 49, "Stuhl6");
+        hatEtikettPlatz[6] = new Etikett(711, 321, 49, 49, "");
         hatEtikettPlatz[6].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(530,380);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[7] = new Etikett(531, 381, 49, 49, "Stuhl7");
+        hatEtikettPlatz[7] = new Etikett(531, 381, 49, 49, "");
         hatEtikettPlatz[7].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(590,380);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[8] = new Etikett(591, 381, 49, 49, "Stuhl8");
+        hatEtikettPlatz[8] = new Etikett(591, 381, 49, 49, "");
         hatEtikettPlatz[8].setzeAusrichtung(Ausrichtung.MITTE);
         
         hatStift.bewegeBis(650,380);
         hatStift.zeichneRechteck(50,50);
-        hatEtikettPlatz[9] = new Etikett(651, 381, 49, 49, "Stuhl9");
+        hatEtikettPlatz[9] = new Etikett(651, 381, 49, 49, "");
         hatEtikettPlatz[9].setzeAusrichtung(Ausrichtung.MITTE);
         
         
@@ -121,14 +122,19 @@ public class PraxisAnwendung extends EBAnwendung
     {
         if ( hatTextName.inhaltAlsText() != "")
         {
+            int lPlatz;
+            do
+            {
+             lPlatz = hatRechner.ganzeZufallszahl(0,9);
+            }while(hatEtikettPlatz[lPlatz].inhaltAlsText() != "");
+
             Patient lPatient;
             lPatient = new Patient(hatTextName.inhaltAlsText());
+            hatEtikettPlatz[lPlatz].setzeInhalt(lPatient.name());
             hatWarteraum.setzePatient(lPatient);
             this.zeigeNaechsterPatient();
             hatZeilenbereich.setzeInhalt(hatWarteraum.toString());
             hatTextName.setzeInhalt("");
-            int lPlatz = hatRechner.ganzeZufallszahl(0,9);
-            hatEtikettPlatz[lPlatz].setzeInhalt(lPatient.name());
         }
     }
 
